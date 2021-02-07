b865c2b54befc6dbdf5c1d3d756e06feeb911de1
Gaspardloriot <gaspardloriot@gmail.com>
2021-02-07 15:39:39 +0100

    feat:handle stream

---
+++ dist/parse.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.csvParser = void 0;
+const fs_1 = __importDefault(require("fs"));
+const csv_parse_1 = __importDefault(require("csv-parse"));
+const getStream = require("get-stream");
+const csvParser = async () => {
+    const filePath = "./src/data.csv";
+    let csvData = [];
+    const stream = fs_1.default
+        .createReadStream(filePath)
+        .pipe(csv_parse_1.default({ delimiter: "," }))
+        .on("data", (csvrow) => {
+        csvData.push(csvrow);
+    })
+        .on("end", () => {
+        const formattedData = csvData;
+        return formattedData;
+    });
+    const formattedData = await getStream.array(stream);
+};
+exports.csvParser = csvParser;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
-import fs from "fs";
-import parse from "csv-parse";
-
-const csvParser = () => {
-  const filePath = "./src/data.csv";
-  let csvData: string[] = [];
-  fs.createReadStream(filePath)
-    .pipe(parse({ delimiter: "," }))
-    .on("data", function (csvrow: string) {
-      csvData.push(csvrow);
-    })
-    .on("end", function () {
-      console.log(csvData[15003]);
-    });
-};
+import { csvParser } from "./parse";
 
 csvParser();
+console.log("hello margaux");
