bec0bf70f8930533a853181b742f53e7fabb116e
Gaspardloriot <gaspardloriot@gmail.com>
2021-02-09 00:07:58 +0100

    feat:handle first file database inclusion

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const parse_1 = require("./parse");
-const index_1 = require("./db/index");
-const getData = async () => {
-    const data = await parse_1.csvParser();
-    console.log(data[4900]);
-};
-getData();
-index_1.launchDatabase();
+const prompt_1 = require("./prompt");
+prompt_1.getFilename();
