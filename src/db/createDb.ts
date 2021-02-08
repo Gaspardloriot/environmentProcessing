91f22bbfbd99d50f2314d4b7c3c47b9d740d4ae5
Gaspardloriot <gaspardloriot@gmail.com>
2021-02-09 00:07:58 +0100

    feat:handle first file database inclusion

---
+++ src/db/createDb.ts
@@ -0,0 +1,15 @@
+import { db } from "./index";
+import { createClientTable } from "./clientTable";
+
+const createDb = (dataBase: string, formattedData: string[]) => {
+  const sql: string = `CREATE DATABASE ${dataBase}db`;
+  db.query(sql, (err: string) => {
+    if (err) throw err;
+    else {
+      createClientTable(dataBase, formattedData);
+      console.log(`Database ${dataBase}db created...`);
+    }
+  });
+};
+
+export { createDb };
