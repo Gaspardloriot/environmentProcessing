1609ccdbd1a9a1d6e5e6352c3a04b2e8c718d0de
Gaspardloriot <gaspardloriot@gmail.com>
2021-03-27 22:36:51 +0100

    feat:add Objects-to-csv

--- src/db/getProjectTable/getProjectTable.ts
+++ src/db/getProjectTable/getProjectTable.ts
@@ -1,6 +1,6 @@
 import { db } from "../index";
 const dataMeta = require("../../../dataStructures.json");
-
+import { getProjectTableInCsv } from "./projectTableCsv";
 const getProjectTable = () => {
   const database: string = dataMeta.dataStructures.database;
   const sql: string = `SELECT * FROM ${database}.PROJECT_DATA_FINAL`;
@@ -8,7 +8,7 @@ const getProjectTable = () => {
   db.query(sql, (err: string, res: string) => {
     if (err) throw err;
     else if (res) {
-      console.log(res[0]);
+      getProjectTableInCsv(res);
     }
   });
 };
--- src/db/getProjectTable/projectTableCsv.ts
+++ src/db/getProjectTable/projectTableCsv.ts
@@ -1,6 +1,11 @@
-const { Parser } = require("json2csv");
+import { csvParser } from "../../parse";
 
-const getProjectTableInCsv = (projectTableJson: Object) => {
-  const json2csvParser = new Parser();
-  const projectTableCsv = json2csvParser.parse(projectTableJson);
+const ObjectsToCsv = require("objects-to-csv");
+
+const getProjectTableInCsv = async (projectTableJson: Object) => {
+  const projectTableCsv = new ObjectsToCsv(projectTableJson);
+
+  await projectTableCsv.toDisk("./project_table.csv");
 };
+
+export { getProjectTableInCsv };
