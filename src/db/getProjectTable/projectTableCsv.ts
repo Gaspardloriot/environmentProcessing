fd338e8b757697b038063e4e5d05b922dcdebdd9
Gaspardloriot <gaspardloriot@gmail.com>
2021-03-27 22:41:59 +0100

    feat: add nodemailer

--- src/db/getProjectTable/projectTableCsv.ts
+++ src/db/getProjectTable/projectTableCsv.ts
@@ -5,7 +5,7 @@ const ObjectsToCsv = require("objects-to-csv");
 const getProjectTableInCsv = async (projectTableJson: Object) => {
   const projectTableCsv = new ObjectsToCsv(projectTableJson);
 
-  await projectTableCsv.toDisk("./project_table.csv");
+  await projectTableCsv.toDisk("./src/db/getProjectTable/project_table.csv");
 };
 
 export { getProjectTableInCsv };
